afcd11e4218d3c4ea1ac9b768f00e20fb4d7bc4a
Raghav Sharma <code.practice17@gmail.com>
2025-12-24 11:55:47 +0530

    Command: Edit parameter names

--- include/command.h
+++ include/command.h
@@ -4,15 +4,15 @@
 
 typedef struct Command
 {
-    char **argv;     /* something like {"ls", "-al", NULL} */
+    char **argv;     /* something like `{"ls", "-al", NULL}` */
     int    argc;     /* argument count including `NULL` */
     int    capacity; /* Capacity of argv array */
 } Command;
 
 
 Command *get_cmd_obj(void);
-void destroy_cmd_obj(Command *command);
-int add_arg_to_cmd_obj(Command *command, const char *arg);
+void destroy_cmd_obj(Command *command_obj);
+int add_arg_to_cmd(Command *command_obj, const char *arg);
 
 
 #endif // COMMAND_H_
--- src/command.c
+++ src/command.c
@@ -7,54 +7,53 @@
 Command *
 get_cmd_obj(void)
 {
-    Command *command = malloc(sizeof(*command));
-    if (command == NULL) {
+    Command *command_obj = malloc(sizeof(*command_obj));
+    if (command_obj == NULL) {
         perror("Command object initialization");
         return NULL;
     }
 
-    command->argv     = NULL;
-    command->argc     = 0;
-    command->capacity = 0;
-    return command;
+    command_obj->argv     = NULL;
+    command_obj->argc     = 0;
+    command_obj->capacity = 0;
+    return command_obj;
 }
 
 
 void
-destroy_cmd_obj(Command *command)
+destroy_cmd_obj(Command *command_obj)
 {
-    for (int i = 0; i < command->argc; i++) {
+    for (int i = 0; i < command_obj->argc; i++) {
         /* Free each array of characters */
-        free(command->argv[i]);
+        free(command_obj->argv[i]);
     }
 
-    free(command);
+    free(command_obj);
 }
 
 
 int
-add_arg_to_cmd_obj(Command *command, const char *arg)
+add_arg_to_cmd(Command *command_obj, const char *arg)
 {
     #define INCR_SIZE 2
 
     /* Expand array if capacity is not sufficient */
-    if (command->capacity <= command->argc) {
-        command->capacity = command->capacity == 0
-                          ? INCR_SIZE : command->capacity + INCR_SIZE;
+    if (command_obj->capacity <= command_obj->argc) {
+        command_obj->capacity += INCR_SIZE;
 
-        char **argv = realloc(command->argv, command->capacity);
+        char **argv = realloc(command_obj->argv, command_obj->capacity);
         if (argv == NULL) {
             perror("Adding arg to command object");
-            command->capacity -= INCR_SIZE;  /* reset capacity */
+            command_obj->capacity -= INCR_SIZE;  /* reset capacity */
             return -1;
         }
 
-        command->argv = argv;
+        command_obj->argv = argv;
     }
 
-    command->argv[command->argc] = arg;
-    command->argc += 1;
-    return 0;
+    command_obj->argv[command_obj->argc] = arg;
+    command_obj->argc += 1;
+    return (command_obj->argc - 1);
 
     #undef INCR_SIZE
 }
